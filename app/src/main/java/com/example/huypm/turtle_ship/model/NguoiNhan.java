f670d562dc289db9d5723c0c9bbe3a63f67e1a71
hhuypm <pmh13797@gmail.com>
2018-11-20 21:39:02 +0700

    NguoiNhan

---
+++ app/src/main/java/com/example/huypm/turtle_ship/model/NguoiNhan.java
@@ -0,0 +1,34 @@
+package com.example.huypm.turtle_ship.model;
+
+public class NguoiNhan {
+    private int Id = -1;
+    private String Ten = "";
+    private String SDT = "";
+
+    public NguoiNhan(){}
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getTen() {
+        return Ten;
+    }
+
+    public void setTen(String ten) {
+        Ten = ten;
+    }
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(String SDT) {
+        this.SDT = SDT;
+    }
+
+}
